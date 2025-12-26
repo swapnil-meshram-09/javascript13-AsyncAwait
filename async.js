e0b2468f69efd1910217122eb7bdc5d1078aaed3
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2025-12-26 19:19:37 +0530

    new commit

---
+++ async.js
@@ -0,0 +1,52 @@
+// // async
+
+// async function test(){
+
+//     // console.log('Testing');                 // promise { undefined }
+
+//     await console.log('Testing');           // promise { <pending> }
+// }
+
+// const r1 = test()
+
+// console.log(r1);                             // without return, it returs a promise with undefined without await keyword
+//                                              // promise { undefined }
+// console.log(typeof test);  
+
+// console.log(typeof r1);
+
+
+
+
+
+
+// async - await
+
+function sum(a,b){
+
+    return a+b
+}
+
+
+async function test2(){
+
+    let a = await sum(2,3);
+
+    let b = await sum(5,5);
+
+    let c = await sum(10,10);
+
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    
+}
+                                 
+const r2 = test2()
+
+// console.log(test2);              // print async function : test2
+
+console.log(r2);
+
+
+
